7d929ebb0edf72e81b7737a0cb2ec75cdc5566a3
heliton vieira <helitondba@gmail.com>
2019-11-06 21:25:10 -0300

    testando app no dispositivo celular

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,5 +1,7 @@
-export const API_CONFIG = {
-  //baseUrl: "http://localhost:8080",
+export const API_CONFIG = {  
   baseUrl: "https://curso-vieirasystem.herokuapp.com",
-  bucketBaseUrl: "https://s3-sa-east-1.amazonaws.com/curso-spring-ionic"
+  bucketBaseUrl: "https://s3-sa-east-1.amazonaws.com/vieirasystem2019"
 }
+
+ //baseUrl: "http://localhost:8080",
+ // bucketBaseUrl: "https://s3-sa-east-1.amazonaws.com/curso-spring-ionic"
